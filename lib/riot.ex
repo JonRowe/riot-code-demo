e8f38d631f19bffa1b471359f5e603839a8ce8ee
Jon Rowe <hello@jonrowe.co.uk>
2019-10-14 15:23:23 +0100

    Combine api and printer peices

--- lib/riot.ex
+++ lib/riot.ex
@@ -2,4 +2,22 @@ defmodule Riot do
   @moduledoc """
   Riot Code Test.
   """
+
+  alias Riot.{MatchState, Printer}
+
+  require Logger
+
+  def find_recent_played_with_matches(name) do
+    name
+    |> MatchState.for_player()
+    |> print()
+  end
+
+  defp print(state = %MatchState{}) do
+    state
+    |> Printer.format_matches_and_players()
+    |> Logger.info()
+  end
+
+  defp print({:error, error}), do: Logger.error(error)
 end
--- test/riot_test.exs
+++ test/riot_test.exs
@@ -1,4 +1,38 @@
 defmodule RiotTest do
-  use ExUnit.Case
+  use Test.Support.RiotCase, async: false
+
+  import ExUnit.CaptureLog
+
   doctest Riot
+
+  describe "find_recent_played_with_matches(name)" do
+    setup %{bypass: bypass} do
+      expect_http(bypass, "/lol/summoner/v4/summoners/by-name/LordTest", "lord_test.json")
+      expect_http(bypass, "/lol/match/v4/matchlists/by-account/ccccddddd", "match_list_2.json")
+      expect_http(bypass, "/lol/match/v4/matches/222", "match_2.json")
+      expect_http(bypass, "/lol/match/v4/matches/333", "match_3.json")
+    end
+
+    test "it will print recent matches" do
+      log =
+        capture_log([colors: [enabled: false]], fn ->
+          Riot.find_recent_played_with_matches("LordTest")
+        end)
+
+      assert log =~
+               "Last 5 matches for: LordTest\n" <>
+                 "\n" <>
+                 "Match: 222\n" <>
+                 "\n" <>
+                 "Players:\n" <>
+                 "OpponentA\n" <>
+                 "OpponentB\n" <>
+                 "\n" <>
+                 "Match: 333\n" <>
+                 "\n" <>
+                 "Players:\n" <>
+                 "OpponentB\n" <>
+                 "OpponentC\n"
+    end
+  end
 end
